7af3772c338572e0e9c850a9d4164eac6c3e1fc4
v2stech@ <adityakv2stech@gmail.com>
2022-12-02 18:29:04 +0530

    Test after change Project Name

---
+++ Template_Implemetation/src/main/java/com/v2stech/fissara/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.v2stech.fissara.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	
+	@GetMapping("/")
+	public ModelAndView displayHomepage(ModelAndView modelAndView) {
+		modelAndView.setViewName("homepage");
+		return modelAndView;
+		
+	}
+
+}
